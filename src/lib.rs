0908c7d27e77f149641a9234719c2d84ffafee83
Élise <elise@eiss.fr>
2024-10-10 09:29:55 +0200

    event loop now created by handler, not by main app can tell chinchilib no more renders/ticks are necessary

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ use std::usize;
 use pixels::{Pixels, SurfaceTexture};
 use winit::window::{Window, WindowId};
 
+/// Mapping for the keys that are recognized. They are centered an AZERTY keyboard's essential keys
+/// needed for games.
 #[derive(Eq, Hash, PartialEq)]
 pub enum MyKeys {
     KeyA,
@@ -49,23 +51,48 @@ pub struct WinitHandler {
     app: Option<Box<dyn GfxApp>>,
 }
 
+fn hz_to_nanosec_period(hz: u16) -> u64 {
+    let nano_period = 1.0 / hz as f64 * 1_000_000_000.0;
+    nano_period as u64
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn hz_to_nanosec_period() {
+        assert_eq!(super::hz_to_nanosec_period(60), 16666666);
+        assert_eq!(super::hz_to_nanosec_period(1), 1_000_000_000);
+    }
+}
+
 impl WinitHandler {
-    pub fn new(app: Box<dyn GfxApp>) -> Self {
+    pub fn new(app: Box<dyn GfxApp>, size: (usize, usize), tick_per_second: u16) -> Self {
+        let nsec_period = hz_to_nanosec_period(tick_per_second);
         Self {
             winfbx: None,
             height: 240,
             width: 320,
             last_frame: std::time::Instant::now(),
-            tick: std::time::Duration::from_nanos(16666666),
+            tick: std::time::Duration::from_nanos(nsec_period),
             app: Some(app),
         }
+    }
+
+    pub fn run(&mut self) -> Result<(), winit::error::EventLoopError> {
+        let event_loop = winit::event_loop::EventLoop::new()?;
+
+        event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+        // event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
+
+        event_loop.run_app(self)?;
+        Ok(())
     }
 }
 
 impl winit::application::ApplicationHandler for WinitHandler {
     /// Resume gets called when window gets loaded for the first time
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        log::info!("resumed called");
+        log::info!(".resumed() called, creating window");
         if let Some(app) = self.app.take() {
             self.winfbx = Some(WinFbx::new(event_loop, self.width, self.height, app));
         }
@@ -73,25 +100,23 @@ impl winit::application::ApplicationHandler for WinitHandler {
 
     /// Instead of redrawing for every event, or every keyprss, we only try to
     /// render after all evens have been processed.
-    fn about_to_wait(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {
+    fn about_to_wait(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
         let app = self
             .winfbx
             .as_mut()
             .expect("about_to_wait not to be called if window doesn't exist.");
 
-        if app.pressed_keys.len() != 0 {
-            // INFO: there is actually noting to do on key still pressed
-            // due to new key management logic
+        if app.done() {
+            event_loop.exit();
+            return;
         }
         let now = std::time::Instant::now();
         let duration_from = now.duration_since(self.last_frame);
         if duration_from >= self.tick {
             self.last_frame = now;
             app.on_tick();
-            log::debug!("requesting redraw from about to wait");
             app.window.request_redraw();
         } else {
-            log::debug!("waiting nothing to do");
             let duration_to = self.tick - duration_from;
             std::thread::sleep(duration_to);
         }
@@ -103,7 +128,6 @@ impl winit::application::ApplicationHandler for WinitHandler {
         _: WindowId,
         event: winit::event::WindowEvent,
     ) {
-        log::debug!("got event {:?}", event);
         let app = self
             .winfbx
             .as_mut()
@@ -182,8 +206,6 @@ impl WinFbx {
     }
 
     fn on_redraw(&mut self) {
-        log::debug!("redrawing");
-
         if self.needs_render {
             self.app.draw(&mut self.pixels, self.width);
 
@@ -195,8 +217,14 @@ impl WinFbx {
         self.needs_render = false;
     }
 
+    fn done(&self) -> bool {
+        self.app.done() == DoneStatus::Exit
+    }
+
     fn on_tick(&mut self) {
-        self.needs_render = self.app.on_tick(&self.pressed_keys);
+        if self.app.done() == DoneStatus::NotDone {
+            self.needs_render = self.app.on_tick(&self.pressed_keys);
+        }
         self.pressed_keys
             .retain(|candidate| !self.released_keys.contains(candidate));
         self.released_keys.clear();
@@ -212,11 +240,7 @@ impl WinFbx {
             if event.state == winit::event::ElementState::Pressed {
                 self.pressed_keys.insert(my_key);
             } else if event.state == winit::event::ElementState::Released {
-                // INFO: released keys are not immediatly removed from pressed keys
-                // as there are yet to be processed by the tick.
-                // They will be removed after the tick.
                 self.released_keys.insert(my_key);
-                // self.pressed_keys.remove(&my_key);
             }
         };
         if event.state == winit::event::ElementState::Pressed {
@@ -240,10 +264,26 @@ impl WinFbx {
     }
 }
 
+#[derive(Eq, PartialEq)]
+pub enum DoneStatus {
+    /// The program should quit, the app has nothing left to do.
+    Exit,
+    /// The program should remain open, but the app is done. Useful when you want the result of the
+    /// app to stay on the screen. On `Remain` the `draw` and `on_tick` methodes will not be called
+    /// anymore.
+    Remain,
+    /// The program should continue, the app is not done.
+    NotDone,
+}
+
 pub trait GfxApp {
     /// Every tick, this method gets called with currently pressed keys. Released keys during the tick are considered still pressed. But will be removed after this call.
     fn on_tick(&mut self, pressed_keys: &std::collections::HashSet<MyKeys>) -> bool;
 
     /// You get the pixel array, so you can draw on it before the render.
     fn draw(&self, pixels: &mut Pixels, width: usize);
+
+    /// Indicate if the app logic is done and if the program should remain or exit. For oneshot
+    /// drawing, return `DoneStatus::Remain` so that the result stays on screen.
+    fn done(&self) -> DoneStatus;
 }
--- src/main.rs
+++ src/main.rs
@@ -64,24 +64,25 @@ impl GfxApp for MovingPixel {
             );
         }
     }
+
+    /// Moving pixel is never done.
+    fn done(&self) -> chinchilib::DoneStatus {
+        if self.pos.0 < 50 {
+            chinchilib::DoneStatus::Remain
+        } else if self.pos.1 < 50 {
+            chinchilib::DoneStatus::Exit
+        } else {
+            chinchilib::DoneStatus::NotDone
+        }
+    }
 }
 
 fn main() {
     env_logger::init();
 
     log::info!("Hello, world!");
-    let event_loop = winit::event_loop::EventLoop::new().unwrap();
 
-    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-    // dispatched any events. This is ideal for games and similar applications.
-    // event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
-
-    // ControlFlow::Wait pauses the event loop if no events are available to process.
-    // This is ideal for non-game applications that only update in response to user
-    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
-
-    let moving_pixel = Box::new(MovingPixel::new(100, 100));
-    let mut app = WinitHandler::new(moving_pixel);
-    event_loop.run_app(&mut app).unwrap();
+    let moving_pixel = Box::new(MovingPixel::new(99, 100));
+    let mut app = WinitHandler::new(moving_pixel, (500, 500), 60);
+    app.run().unwrap();
 }
